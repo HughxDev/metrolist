57aa6db2f84a37c70ffb0067120d4a645df9e512
Hugh Guiney <hugh@hughguiney.com>
2020-03-06 15:54:17 -0500

    Hello World? More like Hello Hell (in Japanese)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <div className="App">
+        <h2><ruby>地獄<rp> (</rp><rt>じごく</rt><rp>)</rp></ruby>、こんにちは！</h2>
+        <p>私の<ruby>給料支払小切手<rp>(</rp><rt>きゅうりょうしはらいこぎって</rt><rp>)</rp></ruby>に<ruby>署名<rp>(</rp><rt>しょめい</rt><rp>)</rp></ruby>する<ruby>愚痴<rp>(</rp><rt>ぐち</rt><rp>)</rp></ruby>っぽいブーマーはメールで私に<ruby>意地悪<rp>(</rp><rt>いじわる</rt><rp>)</rp></ruby>だったよ。</p>
+      </div>
     </div>
   );
 }
