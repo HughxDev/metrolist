2429f494d37bbb87c1b21f2bf6e436729391d414
Hugh Guiney <hugh@hughguiney.com>
2020-03-12 17:37:31 -0400

    Add IconCard component

---
+++ src/components/IconCard/IconCard.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import IconCard from './index';
+
+export default {
+  "title": "IconCard",
+  "icon-card": IconCard,
+};
+
+export const Default = () => <IconCard />;
---
+++ src/components/IconCard/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Icon from '../Icon';
+
+import './IconCard.scss';
+
+function IconCard( props ) {
+  return (
+    <a className="cob-icon-card" href={ props.href }>
+      <Icon className="cob-icon-card__image" icon={ props.icon } width="50" />
+      <span className="cob-icon-card__text">{ props.children }</span>
+    </a>
+  );
+}
+
+IconCard.propTypes = {
+  "children": PropTypes.node,
+  "icon": PropTypes.string,
+  "href": PropTypes.string,
+};
+
+export default IconCard;
