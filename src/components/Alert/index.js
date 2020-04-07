c191b3f4d598989dc798d4c2defabe0bab104d19
Hugh Guiney <hugh@hughguiney.com>
2020-04-07 17:04:00 -0400

    Add Alert component

---
+++ src/components/Alert/index.js
@@ -0,0 +1,27 @@
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+
+import './Alert.scss';
+
+const Alert = forwardRef( ( props, ref ) => (
+    <div
+      { ...props }
+      ref={ ref }
+      className={ `ml-alert${props.variant && ` ml-alert--${props.variant}`}${props.className ? ` ${props.className}` : ''}` }
+      role="alert"
+      aria-live="polite"
+    >
+      { props.children }
+    </div>
+) );
+
+Alert.displayName = 'Alert';
+
+Alert.propTypes = {
+  "id": PropTypes.string,
+  "variant": PropTypes.string,
+  "children": PropTypes.node,
+  "className": PropTypes.string,
+};
+
+export default Alert;
