fbc94a74d88dc9a671beb07719f9056bde43b166
Hugh Guiney <hugh@hughguiney.com>
2020-04-17 15:00:01 -0400

    AMI Estimation algorithm

--- src/components/AmiCalculator/_AmiCalculatorResult/index.js
+++ src/components/AmiCalculator/_AmiCalculatorResult/index.js
@@ -1,61 +1,54 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, forwardRef } from 'react';
 import PropTypes from 'prop-types';
 
 import Button from '@components/Button';
 import Stack from '@components/Stack';
 import InputSummary from '../_AmiCalculatorInputSummary';
 
-import sampleApiResponse from './sample-api-response.json';
-// import sampleApiResponse2 from './sample-api-response-2.json';
-
 import './AmiCalculatorResult.scss';
 
-function AmiCalculatorResult( props ) {
-  function estimateAmi( { householdSize, householdIncome, incomeRate } ) {
-    const parsedHouseholdSize = parseInt( householdSize, 10 );
-    const deformattedHouseholdIncome = householdIncome.replace( /[$,]/g, '' );
-    let parsedHouseholdIncome = parseFloat( deformattedHouseholdIncome );
+// 100% AMI
+const amiDefinition = {
+  "people_1": 79350,
+  "people_2": 90650,
+  "people_3": 102000,
+  "people_4": 113300,
+  "people_5": 122400,
+  "people_6": 131450,
+};
 
-    if ( incomeRate === 'Monthly' ) {
-      parsedHouseholdIncome *= 12;
-    }
+function estimateAmi( { householdSize, householdIncome, incomeRate } ) {
+  // const parsedHouseholdSize = parseInt( householdSize, 10 );
+  // const deformattedHouseholdIncome = householdIncome.replace( /[$,]/g, '' );
+  householdIncome = parseFloat( householdIncome.value.replace( /[$,]/g, '' ) );
+  const maxIncome = amiDefinition[`people_${householdSize.value}`];
 
-    return (
-      sampleApiResponse
-        .map( ( amiBracket ) => {
-          amiBracket.AMIPercent = parseInt( amiBracket.AMIPercent, 10 );
-          amiBracket.people = parseInt( amiBracket.people.trim(), 10 );
-          amiBracket.maxIncome = parseInt( amiBracket.maxIncome, 10 );
-
-          return amiBracket;
-        } )
-        .filter( ( amiBracket ) => ( parsedHouseholdSize === amiBracket.people ) )
-        .filter( ( amiBracket ) => ( parsedHouseholdIncome < amiBracket.maxIncome ) )
-        .sort( ( a, b ) => b.AMIPercent - a.AMIPercent )
-    );
+  if ( incomeRate.value === 'Monthly' ) {
+    householdIncome *= 12;
   }
 
-  useEffect( () => {
-    props.setStep( props.step );
-    console.log( 'props.formData', props.formData );
-    console.log( 'sampleApiResponse', sampleApiResponse );
-    // console.log( 'sampleApiResponse2', sampleApiResponse2 );
+  return Math.floor( ( householdIncome / maxIncome ) * 100 );
+}
 
-    const estimation = estimateAmi( {
-      "householdSize": 4,
-      "householdIncome": "$5,000.00",
-      "incomeRate": "Monthly",
-    } );
+const AmiCalculatorResult = forwardRef( ( props, ref ) => {
+  const data = props.formData.householdSize.value ? props.formData : props.fakeFormData;
+  const [amiEstimation, setAmiEstimation] = useState( estimateAmi( data ) );
 
-    console.log( 'estimation', estimation );
-  }, [] );
+  useEffect( () => props.setStep( props.step ), [] );
+
+  // useEffect( () => setAmiEstimation( estimateAmi( {
+  //   "householdSize": props.formData.householdSize.value,
+  //   "householdIncome": props.formData.householdIncome.value,
+  //   "incomeRate": props.formData.incomeRate.value,
+  // } ) ), [props.formData] );
+
+  console.log( props.formData );
 
   return (
-    <Stack space="2" className={ `ml-ami-calculator__result${props.className ? ` ${props.className}` : ''}` }>
+    <Stack ref={ ref } space="2" className={ `ml-ami-calculator__result ml-ami-calculator__prompt${props.className ? ` ${props.className}` : ''}` }>
       <InputSummary formData={ props.formData } />
       <Stack space="1">
-        <p><span className="ml-all-caps">Estimated Eligibility:</span> <dfn className="ml-ami">54% AMI</dfn> (Area Median Income)</p>
-        <p>54% qualifies you for homes listed at <b className="ml-ami">60% AMI</b> and above.</p>
+        <p><span className="ml-all-caps">Estimated Eligibility:</span> <dfn className="ml-ami">{ amiEstimation }% AMI</dfn> (Area Median Income)</p>
       </Stack>
       <Stack as="nav" space="1">
         <Button as="a" variant="primary">See homes that match your eligibility</Button>
@@ -63,7 +56,9 @@ function AmiCalculatorResult( props ) {
       </Stack>
     </Stack>
   );
-}
+} );
+
+AmiCalculatorResult.displayName = 'Result';
 
 AmiCalculatorResult.propTypes = {
   "children": PropTypes.node,
@@ -71,6 +66,21 @@ AmiCalculatorResult.propTypes = {
   "step": PropTypes.number,
   "setStep": PropTypes.func,
   "formData": PropTypes.object,
+  "fakeFormData": PropTypes.object,
+};
+
+AmiCalculatorResult.defaultProps = {
+  "fakeFormData": {
+    "householdSize": {
+      "value": 4,
+    },
+    "householdIncome": {
+      "value": "$5,000.00",
+    },
+    "incomeRate": {
+      "value": "Monthly",
+    },
+  },
 };
 
 export default AmiCalculatorResult;
