ad7e04d69b7efc919ab0cd92146a398ac4fdee07
Hugh Guiney <hugh@hughguiney.com>
2020-07-08 15:47:14 -0400

    Add isPlainObject util

--- src/util/objects.js
+++ src/util/objects.js
@@ -5,3 +5,11 @@
 export function hasOwnProperty( object, property ) { // eslint-disable-line import/prefer-default-export
   return Object.prototype.hasOwnProperty.call( object, property );
 }
+
+export function isPlainObject( value ) {
+  return (
+    ( value !== null )
+    && ( typeof value === "object" )
+    && ( Object.getPrototypeOf( value ) === Object.prototype )
+  );
+}
